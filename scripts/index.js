f0b96c0499165ca3f5c473f17c7fd62b63c2e21d
Leon Huston <lhuston848@gmail.com>
2018-05-19 22:05:18 +1200

    Implemented base map function

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-function getGeoCode(latitude, longitude){
+function getGeoCode(latitude, longitude) {
     var lat = latitude;
     var long = longitude;
     var responseJSON;
@@ -9,7 +9,7 @@ function getGeoCode(latitude, longitude){
     xhr.open("GET", uri, true);
     xhr.setRequestHeader('Accept', 'application/json');
     xhr.setRequestHeader('user-key', 'a5d7d8b6e3bcbabee209e2b105999150');
-    
+
     xhr.onload = function () {
         responseJSON = JSON.parse(xhr.responseText);
         console.log(responseJSON);
@@ -19,7 +19,26 @@ function getGeoCode(latitude, longitude){
 
 }
 
-function getMenu(id){
+function getMap(latCode, lonCode) {
+    var map = new ol.Map({
+        target: 'map',
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM()
+            }),
+            new ol.layer.Vector({
+                source: new ol.source.Vector(),
+                style: new ol.style.Style()
+            })
+        ],
+        view: new ol.View({
+            center: ol.proj.fromLonLat([lonCode, latCode]),
+            zoom: 18
+        })
+    })
+}
+
+function getMenu(id) {
 
     var menuJSON;
     menu_uri = 'https://developers.zomato.com/api/v2.1/dailymenu?res_id=' + id;
@@ -27,15 +46,15 @@ function getMenu(id){
     xhr2.open("GET", menu_uri, true);
     xhr2.setRequestHeader('Accept', 'application/json');
     xhr2.setRequestHeader('user-key', 'a5d7d8b6e3bcbabee209e2b105999150');
-    xhr2.onload = function (){
+    xhr2.onload = function () {
         menuJSON = JSON.parse(xhr2.responseText);
     }
     xhr2.send(null);
     return menuJSON;
 }
 
-function setInfo(responseJSON){
-    for(var i = 0; i <responseJSON.nearby_restaurants.length; i++){
+function setInfo(responseJSON) {
+    for (var i = 0; i < responseJSON.nearby_restaurants.length; i++) {
 
         console.log('name:', responseJSON.nearby_restaurants[i].restaurant.name);
         console.log('address:', responseJSON.nearby_restaurants[i].restaurant.location.address);
@@ -45,7 +64,7 @@ function setInfo(responseJSON){
         console.log('photo url:', responseJSON.nearby_restaurants[i].restaurant.featured_image);
         console.log('menu:', getMenu(responseJSON.nearby_restaurants[i].restaurant.id));
         console.log('-------------------------')
-        
+
     }
 }
 
