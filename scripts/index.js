28f9d1148c89db103e23da2876d79c4279ce454d
Layton Wright <layton.wright9@gmail.com>
2018-05-19 12:05:01 +1200

    working on api request

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,13 @@
-function getGeoCode(lat, long){
-    var uri = "https://developers.zomato.com/api/v2.1/geocode?lat${lat}&lon=${long}";
+function getGeoCode(latitude, longitude){
+    let lat = latitude;
+    let long = longitude;
+    var uri = 'https://developers.zomato.com/api/v2.1/geocode?lat=' + lat + '&lon=' + long;
 
     var xhr = new XMLHttpRequest();
     xhr.open("GET", uri, true);
+    xhr.setRequestHeader('Accept', 'application/json');
     xhr.setRequestHeader('user-key', 'a5d7d8b6e3bcbabee209e2b105999150');
+    
     xhr.onload = function () {
         var responseJSON = JSON.parse(xhr.responseText);
         console.log(responseJSON);
